51d130c65ce694bd9366295bf167bda7f738033e
yql <1014796661@qq.com>
2023-01-05 15:41:09 +0800

    master test

--- src/main/java/com/yql/git/GitTest.java
+++ src/main/java/com/yql/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hot-fix");
         System.out.println("hot-fix");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix test");
     }
 }
