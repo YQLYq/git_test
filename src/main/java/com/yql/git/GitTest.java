997f2ab88eb5e9ec40695d0782afc23b903bb250
YQLYq <90961651+YQLYq@users.noreply.github.com>
2023-01-05 15:57:41 +0800

    github test

--- src/main/java/com/yql/git/GitTest.java
+++ src/main/java/com/yql/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("hot-fix test");
         System.out.println("github");
+        System.out.println("github test");
     }
 }
